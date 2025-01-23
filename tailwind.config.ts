e4d3cf51e2cc49463db3d6aa42f38d98ef03aaf8
Manish <manishguhe301@gmail.com>
2025-01-23 19:40:52 +0530

    feat: spinner for loading, updated config, and created the styles for the spinner, + assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,8 @@ export default {
       boxShadow: {
         'custom-orange': '0 10px 10px 0 #FBA65140',
         'custom-orange-l3': '0 0 15px 0 rgba(251, 166, 81, 0.5)',
+        'custom-orange-l2': '0 0 7px 0 rgba(176, 116, 57, 0.2)',
+
       },
       height: {
         'full-screen-minus-header': 'calc(100vh - 60px)',
